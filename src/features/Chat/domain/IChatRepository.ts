24e78e1c6472fe264eabdd548e3f6d8460d278bf
Dylanyessid <dylanyessid1904@gmail.com>
2025-03-03 10:47:43 -0500

    feat(chat): Chat repository implemented

---
+++ src/features/Chat/domain/IChatRepository.ts
@@ -0,0 +1,5 @@
+import { Chat } from "./Chat";
+
+export interface IChatReposiory{
+    create:(chat:Chat)=>Promise<Chat|null>
+}
---
+++ src/features/Chat/infrastructure/ChatRepository.ts
@@ -0,0 +1,11 @@
+import { Chat } from "../domain/Chat";
+import { IChatReposiory } from "../domain/IChatRepository";
+
+class ChatRepository implements IChatReposiory {
+    
+    create(chat: Chat) {
+        
+    }
+}
+
+export default ChatRepository
